786ff6858ddf84a5f940b462c7c34f105652a7bc
komald4 <komald.4@gmail.com>
2020-03-19 10:44:09 -0400

    urlInterpolationRemoval

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
+  getImage() //this is defining the function to use to make my code easier to read for the team/readers
+}) 
+
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 'https://randopic.herokuapp.com/images/4875'
+  let imageId = '4875' //had to remove url link as it created a duplication during interpolation below
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
   const likeURL = `https://randopic.herokuapp.com/likes/`
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+ 
 
-})
+  
+const getImage = () => {
+  fetch(imageURL, {method: 'Get'})
+  .then(response => response.json())
+  .then(images => {
+    renderImages(images)
+  })
+  }
+
+  const renderImages = images => {
+    console.log(images)
+  }
+
 
 //get the image data. this is done by using fetch imageURL from the website given
 //render image to the website
 
-document.addEventListener("DOMContentLoaded", event => {
-  getImage() //this is defining the function to use to make my code easier to read for the team/readers
-})
 
-const getImage = () => {
-  fetch(imageURL)
-  .then(response => response.json())
-  .then(console.log)
-  }
+
