c8db224c60ec3a3625e061d4fe91e34a5da000f2
wags83 <gpwagner@gmail.com>
2020-03-19 09:50:39 -0400

    9:50

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+	console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+	const imageId = 4867 // Enter the id from the fetched image here
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+	const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
+	const likeURL = `https://randopic.herokuapp.com/likes/`
 
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+	const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+	function getImage() {
+		fetch('https://randopic.herokuapp.com/images/4867')
+			.then(response => {
+				console.log(response)
+				response.json()
+			})
+			.then(result => {
+				console.log(result)
+			})
+	}
+
+	getImage()
 })
