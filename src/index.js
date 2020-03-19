e9301e33b9f31af9cb252bf155c2c525119b340e
github username <github email address>
2020-03-19 10:11:57 -0400

    layout complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
+// on load user should see an image, comments for the image, and number of likes
+// user can click a button to like image. like count should increase by 1 without refresh
+// user can submit comment via form, comments should append to existing list of comments
+// likes and comments must persist to the db
+
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
-
-  let imageId = 1 //Enter the id from the fetched image here
-
+  
+  let imageId = 4884 //Enter the id from the fetched image here
+  
+  fetchImage(imageId)
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
-
+  
   const likeURL = `https://randopic.herokuapp.com/likes/`
-
+  
   const commentsURL = `https://randopic.herokuapp.com/comments/`
-
+  
 })
+
+let image = document.getElementById('image')
+let imageCard = document.getElementById('image_card')
+let imageTitle = document.getElementById('name')
+let likes = document.getElementById('likes')
+let likeButton = document.getElementById('like_button')
+let form = document.getElementById('comment_form')
+let formField = document.getElementById('comment_input')
+let commentsUl = document.getElementById('comments')
+
+function fetchImage(imageId) {
+  fetch(`https://randopic.herokuapp.com/images/${imageId}`)
+  .then(response => response.json())
+  .then(imageResponse => {
+    image.src = imageResponse.url 
+    imageTitle.innerText = imageResponse.name
+    likes.innerText = imageResponse.like_count
+    imageResponse.comments.forEach(comment => {
+      let commentLi = document.createElement('li')
+      commentLi.innerText = comment
+      commentsUl.append(commentLi)
+    })
+    imageCard.append(image, imageTitle, likes, likeButton, form, commentsUl)
+  })
+}
