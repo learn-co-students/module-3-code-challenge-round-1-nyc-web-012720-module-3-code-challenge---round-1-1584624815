71e710dd9ab753f38219bf5617e4c49d7be7e3d2
github username <github email address>
2020-03-19 09:42:03 -0400

    start code challenge

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta');
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 4860; //Enter the id from the fetched image here
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`;
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
+  const likeURL = `https://randopic.herokuapp.com/likes/`;
 
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+  const commentsURL = `https://randopic.herokuapp.com/comments/`;
 
-})
+});
+
+/*
+step1.
+  1.Get request to load image on page.
+*/
+
+document.addEventListener("DOMContentLoaded", function ()}
+  renderImage()
+);
