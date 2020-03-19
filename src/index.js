03c81849188843ede9a0c25a6e1d7b19289aed8e
sekoudosso82 <sekou.dosso82@gmail.com>
2020-03-19 10:42:36 -0400

    2nd commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
   let imageId = 4885 //Enter the id from the fetched image here
 
@@ -9,4 +9,48 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
   // start
+
+   fetchImages()
+
+
+  let body = document.getElementsByTagName('body')
+// fetch all images
+function fetchImages(){
+  fetch(imageURL)
+  .then(res => res.json())
+  .then(function(image){
+    
+      addImage(image)
+    
+  })
+}
+
+// add image to the browser
+function addImage(image){
+  console.log(image.url)
+  // let newDiv = document.createElement('div')
+  body.innerHTML = `<div class="container">
+  <div class="row" id="image_content">
+    <div class="card col-md-4"></div>
+    <div id="image_card" class="card col-md-4">
+        <img src=${image.url} id="image" data-id=${image.id}/>
+        <h4 id="name">${image.name}</h4>
+        <span>Likes:
+          <span id="likes">${image.like_count}</span>
+        </span>
+        <button id="like_button">Like</button>
+        <form id="comment_form">
+          <input id="comment_input" type="text" name="comment" placeholder="Add Comment"/>
+          <input type="submit" value="Submit"/>
+        </form>
+        <ul id="comments">
+             <li> ${image.comment} </li>
+        </ul>
+      </div>
+    <div class="card col-md-4"></div>
+  </div>
+</div>
+  `
+}
+
 })
