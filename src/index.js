433054367d1691f3e8861c6ff76844d87f58af86
github username <github email address>
2020-03-19 11:12:03 -0400

    deliverables + bonus:

--- src/index.js
+++ src/index.js
@@ -9,69 +9,23 @@ let imageCard = document.getElementById('image_card')
 let imageTitle = document.getElementById('name')
 let likes = document.getElementById('likes')
 let likeButton = document.getElementById('like_button')
-likeButton.classname = 'like-btn'
+likeButton.className = 'like-btn'
 let form = document.getElementById('comment_form')
 let formField = document.getElementById('comment_input')
 let commentsUl = document.getElementById('comments')
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
-  
-  
-  fetchLayout(imageId)
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
-  
   const likeURL = `https://randopic.herokuapp.com/likes/`
-  
+  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
   const commentsURL = `https://randopic.herokuapp.com/comments/`
-  
+  fetchLayout(imageURL)
+  likeImage(likeURL)
+  commentImage(commentsURL)
+  deleteComment(commentsURL)
 })
 
-imageCard.addEventListener('click', function(event) {
-  if (event.target.classname === 'like-btn') {
-    let likeCount = event.target.parentNode.querySelector('#likes').innerText
-    event.target.parentNode.querySelector('#likes').innerText = parseInt(likeCount) + 1
-
-    fetch('https://randopic.herokuapp.com/likes', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify({
-        image_id: imageId
-      })
-    })
-  }
-})
-
-form.addEventListener('submit', function(event) {
-  event.preventDefault()
-  let newCommentLi = document.createElement('li')
-  newCommentLi.innerText = formField.value 
-  commentsUl.append(newCommentLi)
-  formField.value = ""
-
-  fetch('https://randopic.herokuapp.com/comments', {
-    method: 'POST', 
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    },
-    body: JSON.stringify({
-      image_id: imageId,
-      content: event.target.parentNode.querySelector('ul').lastChild.innerText
-    })
-  })
-  .then(response => response.json())
-  .then(comment => {
-    console.log(comment)
-  })
-})
-
-
-function fetchLayout(imageId) {
-  fetch(`https://randopic.herokuapp.com/images/${imageId}`)
+function fetchLayout(imageURL) {
+  fetch(imageURL)
   .then(response => response.json())
   .then(imageResponse => {
     image.src = imageResponse.url 
@@ -79,10 +33,67 @@ function fetchLayout(imageId) {
     likes.innerText = imageResponse.like_count
     imageResponse.comments.forEach(comment => {
       let commentLi = document.createElement('li')
+      let deleteButton = document.createElement('button')
       commentLi.innerText = comment.content
+      deleteButton.innerText = 'x'
+      deleteButton.className = 'delete-btn'
+      deleteButton.dataset.id = comment.id
+      
+      commentLi.appendChild(deleteButton)
       commentsUl.append(commentLi)
     })
     imageCard.append(image, imageTitle, likes, likeButton, form, commentsUl)
+  })
+}
+
+function likeImage(likeURL) {
+  imageCard.addEventListener('click', function(event) {
+    if (event.target.className === 'like-btn') {
+      let likeCount = event.target.parentNode.querySelector('#likes').innerText
+      event.target.parentNode.querySelector('#likes').innerText = parseInt(likeCount) + 1
+  
+      fetch(likeURL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+          image_id: imageId
+        })
+      })
+    }
+  })
+}
+
+function commentImage(commentsURL) {
+  form.addEventListener('submit', function(event) {
+    event.preventDefault()
+    let newCommentLi = document.createElement('li')
+    newCommentLi.innerText = formField.value 
+    commentsUl.append(newCommentLi)
+    formField.value = ""
+  
+    fetch(commentsURL, {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        image_id: imageId,
+        content: event.target.parentNode.querySelector('ul').lastChild.innerText
+      })
+    })
+  })
+}
+
+function deleteComment(commentsURL){
+  commentsUl.addEventListener('click', function(event) {
+    if (event.target.className === 'delete-btn'){
+      event.target.parentNode.remove()
+      fetch(`${commentsURL}/${event.target.dataset.id}`, {method: 'DELETE'})
+    }
   })
 }
 
