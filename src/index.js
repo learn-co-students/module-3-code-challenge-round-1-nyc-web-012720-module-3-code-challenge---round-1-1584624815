730b2b858481d62873ff7dbb1063e38bfa7838bf
github username <github email address>
2020-03-19 10:03:22 -0400

    first 20mins

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 
 /*
-step1.
+step 1.
   1.[√]Get request to load image on page.
+  2.[√]display name of image
+step 2.
+  1.[]Add like to picture
+  2.[]As 'click' event, increase the number of likes
 */
 let fetchImage;
+let imageData;
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta');
@@ -23,6 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
         return response.json();
       })
       .then(function (image) {
+        imageData = image;
         renderImage(image);
       });
   };
@@ -30,9 +36,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
+document.body.addEventListener("click", function (event) {
+  if (event.target.id === "like_button") {
+    addLikes(event.target);
+  }
+});
+
 function renderImage(image) {
+
   let img = document.getElementById("image");
-  return img.src = image.url;
+  let h4 = document.getElementById("name");
+
+  img.src = image.url;
+  h4.innerText = image.name;
+
 }
 
+function addLikes(btn) {
+  let span = btn.parentNode.querySelector("#likes");
+  btn.dataset.imageId = imageData.id;
 
+  span.innerText = 0;
+  span.innerText += 1;
+}
