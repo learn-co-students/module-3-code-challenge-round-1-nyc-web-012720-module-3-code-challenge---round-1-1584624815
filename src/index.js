31c7df5748568d616da9675f025bcb9760f70936
haltonba <haltonba94@gmail.com>
2020-03-19 10:02:51 -0400

    Second Commit Fam

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 4874
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+  const imageURL = `https://randopic.herokuapp.com/images/4874`
 
   const likeURL = `https://randopic.herokuapp.com/likes/`
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  fetchImage()
+
 })
 
-function name(params) {
+function fetchImage() {
+  fetch(imageURL)
+  .then(response => response.json())
+  .then(json => renderImage(json))
+}
+
+function renderImage(image) {
+  const imageCard = document.getElementById("image_card")
   
+  imageCard.innerHTML = `
+    <img src="${image.url}" id="image" data-id="${image.id}"/>
+    <h4 id="name">${image.name}</h4>
+    <span>Likes:
+      <span id="likes">${like_count}</span>
+    </span>
+    <button id="like_button">Like</button>
+    <form id="comment_form">
+      <input id="comment_input" type="text" name="comment" placeholder="Add Comment"/>
+      <input type="submit" value="Submit"/>
+    </form>
+    <ul id="comments">
+        <!-- <li> for each comment goes here -->
+    </ul>
+  `
 }
