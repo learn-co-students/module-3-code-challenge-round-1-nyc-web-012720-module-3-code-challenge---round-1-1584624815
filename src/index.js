6dc9cea69378b3844bcbe02a59c69a870f36806b
Jorge Basilio <J.l.Basilio55@gmail.com>
2020-03-19 10:22:20 -0400

    third commit

--- src/index.js
+++ src/index.js
@@ -2,24 +2,29 @@ let imageId = 4883 //Enter the id from the fetched image here
 const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 const likeURL = `https://randopic.herokuapp.com/likes/`
 const commentsURL = `https://randopic.herokuapp.com/comments/`
-const getImageId = document.getElementById("image")
+const getLikeId = document.getElementById("likes")
 
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
-
+  
   getImage = () => {
     fetch(imageURL)
     .then(response => response.json())
-    .then(image => {
-      console.log(image)
-      renderImage(image)
-    
+    .then(response => {
+      renderImage(response.url)
+      setLikes(response.like_count)
+      
     })
   }
-
+  
   const renderImage = image => {
-    getImageId.src=`${image}`
+    const getImageId = document.getElementById("image")
+    getImageId+=`img src="${image}" id="image" data-id=""/>`
+  }
+  
+  const setLikes = likes => {
+    getLikeId.innerText = `${like}`
   }
 
 })
