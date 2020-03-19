d9eb79b798de5e6d63f7b43fde7719530f94ab98
wags83 <gpwagner@gmail.com>
2020-03-19 10:14:28 -0400

    10:15

--- src/index.js
+++ src/index.js
@@ -9,16 +9,54 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	const commentsURL = `https://randopic.herokuapp.com/comments/`
 
-	function getImage() {
-		fetch('https://randopic.herokuapp.com/images/4867')
-			.then(response => {
-				console.log(response)
-				response.json()
-			})
-			.then(result => {
-				console.log(result)
-			})
-	}
-
 	getImage()
+
+	document.addEventListener('click', event => {
+		console.log('click')
+		if (event.target.id === 'like_button') {
+			console.log('like click')
+			addLike()
+		}
+	})
 })
+
+function getImage() {
+	fetch('https://randopic.herokuapp.com/images/4867')
+		.then(response => {
+			return response.json()
+		})
+		.then(result => {
+			renderImage(result)
+		})
+}
+
+function renderImage(image) {
+	const imageDiv = document.querySelector('.row')
+	imageDiv.innerHTML = `
+  <img src="${image.url}" id="image" data-id="${image.id}"/>
+  <h4 id="name">${image.name}</h4>
+  <span>Likes:
+    <span id="likes">0</span>
+  </span>
+  <button id="like_button">Like</button>
+  <form id="comment_form">
+    <input id="comment_input" type="text" name="comment" placeholder="Add Comment"/>
+    <input type="submit" value="Submit"/>
+  </form>
+  <ul id="comments">
+  </ul>`
+}
+
+function addLike() {
+	const likes = document.querySelector('#likes')
+	likes.textContent++
+}
+
+function postLike() {
+  postObject = {
+    method: 'POST',
+    headers: {
+      'content-type'
+    }
+  }
+}
