8380b49c559cc373d4d762a5482c093e8e9141e9
github username <github email address>
2020-03-19 09:22:10 -0500

    add like functionality

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
   const likeURL = `https://randopic.herokuapp.com/likes/`
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+  const imgEl = document.querySelector("#image");
+  const h4 = document.querySelector("#name");
+  const span = document.querySelector("#likes");
+  const commentList = document.querySelector("#comments");
+  const likeButton = document.querySelector("#like_button");
+  const commentForm = document.querySelector("#comment_form");
+  const commentValue = document.querySelector("#comment_input").value;
 
   // <div class="container">
   //   <div class="row" id="image_content">
@@ -28,14 +35,27 @@ document.addEventListener('DOMContentLoaded', () => {
   //   </div>
   // </div>
 
+//   Example Response:
+// {
+//   "id": 1,
+//   "url": "http://blog.flatironschool.com/wp-content/uploads/2016/07/072716-js-saved-web-4-352x200.jpg",
+//   "name": "The Internet!",
+//   "like_count": 0,
+//   "comments": [
+//     {
+//       "id": 5941,
+//       "content": "first comment!",
+//       "image_id": 1158,
+//       "created_at": "2018-10-18T17:06:14.859Z",
+//       "updated_at": "2018-10-18T17:06:14.859Z"
+//     }
+//   ]
+// }
+
   const renderImg = (img) => {
-    const imgEl = document.querySelector("#image");
     imgEl.setAttribute("src", img.url);
-    const h4 = document.querySelector("#name");
     h4.innerHTML = img.name;
-    const span = document.querySelector("#likes");
     span.innerHTML = img.like_count;
-    const commentList = document.querySelector("#comments");
     img.comments.forEach(comment => {
       const li = document.createElement("li");
       li.innerHTML = comment.content;
@@ -51,9 +71,49 @@ document.addEventListener('DOMContentLoaded', () => {
       .catch(err => console.log(err));
   }
 
-  const addLike = () => {
-    
+  const addLikeBack = () => {
+    newLikeObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({image_id: imageId})
+    };
+
+    fetch(likeURL, newLikeObj)
+      .then(res => res.json())
+      .then(like => console.log(like))
+      .catch(err => console.log(err));
   }
 
+  const addLikeFront = () => {
+    let likeCount = parseInt(span.innerHTML, 10);
+    likeCount++;
+    span.innerHTML = likeCount;
+  }
+
+
+  const addCommentBack = () => {
+
+  }
+
+
+  const addCommentFront = () => {
+    const li = document.createElement("li");
+    li.innerHTML = commentValue;
+    commentList.appendChild(li);
+  }
+
+
   getImage();
+  likeButton.onclick = () => {
+    addLikeFront();
+    addLikeBack();
+  }
+  commentForm.onsubmit = (e) => {
+    e.preventDefault();
+    addCommentFront();
+
+  }
 })
