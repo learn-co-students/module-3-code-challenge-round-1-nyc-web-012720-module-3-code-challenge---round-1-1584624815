0ebf6f180ef1502d28c6196082bb08f4624702a7
Babyyodabc <rjwahba@gmail.com>
2020-03-19 13:34:25 -0400

    done

--- src/index.js
+++ src/index.js
@@ -2,19 +2,18 @@ let imageId = 4861
 
 const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
+const likeURL = `https://randopic.herokuapp.com/likes/`
+
+const commentsURL = `https://randopic.herokuapp.com/comments/`
+
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
-
- 
-
-  const likeURL = `https://randopic.herokuapp.com/likes/`
-
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
 
   //call functions
   getImage();
   newComment();
-  // likePic();
+  likePic();
 
 })
 
@@ -35,52 +34,54 @@ function renderImage(object) {
       <span>Likes:
             <span id="likes">${object.like_count}</span>
           </span>
-          <button id="like_button">Like</button>
-          <form id="comment_form">
+      <button id="like_button">Like</button>
+      <form class="comment_form">
           <input id="comment_input" type="text" name="comment" placeholder="Add Comment"/>
           <input type="submit" value="Submit"/>
-        </form>
+      </form>
       <ul id="comments">
-        <li> ${object.comments.forEach( x => { x.content.value})} </li>
+      
       </ul>
   `
+    object.comments.forEach( x => {
+      const ul = document.getElementById("comments")
+      const li = document.createElement("li")
+      li.innerText = `${x.content}`
+      ul.appendChild(li)
+    })
 }
 
-// function likePic() {
-    // const likeButtton = document.getElementById("like_button")
-    // const span = document.getElementById("new-likes")
-    // console.log(span.innerText)
-
+function likePic() {
+  document.addEventListener("click", event => {
+    if (event.target.id === "like_button") {
+      console.log(event.target)
+      let span = document.getElementById("likes")
+      span.innerText = `${++span.innerText}`
     // was having trouble getting the actual number of likes, despite changing tag names, ids, etc.
     //the idea is to get like amount thorough a split on the inner text then increment with ++
     //and append new score to page. since it is optimistic, the fetch( database persistence) comes after the dom is updated
-
-//     document.addEventListener("click", event => {
-//       if (event.target.id === "like_button") {
-//         fetch('https://randopic.herokuapp.com/likes', {
-//           method: "POST",
-//           headers: {
-//             "content-type": "application/json",
-//             "accept": "application/json"
-//           },
-//           body: JSON.stringify({ image_id: `${imageId}`})
-      
-//         })
-//       }
-//     })
-// }
+ 
+        fetch('https://randopic.herokuapp.com/likes', {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+            "accept": "application/json"
+          },
+          body: JSON.stringify({ image_id: 4861})
+        })
+      }})
+      }
 
 function newComment() {
-  const form = document.getElementById("comment_form")
-      form.addEventListener("submit", event => {
-        event.preventDefault()
+      document.addEventListener("submit", event => {
+        if (event.target.className === "comment_form") {
+          event.preventDefault()
+        
         const commentList = document.getElementById("comments")
         let newComm = document.createElement("li")
-        
-        console.log(event.target)
-        newComm = event.target.comment.value
+        newComm.innerText = event.target.comment.value
         commentList.appendChild(newComm)
-
+      
         fetch('https://randopic.herokuapp.com/comments', {
           method: "POST",
           headers: {
@@ -88,9 +89,11 @@ function newComment() {
           'Content-Type': 'application/json'
           },
           body: JSON.stringify({ 
-            image_id: "",
-           content: ""  })
+            image_id: 4861,
+           content: event.target.comment.value  })
         })
-        // .then( //logic to append to dom))
+        event.target.reset()
+      }
       })
     }
+  
