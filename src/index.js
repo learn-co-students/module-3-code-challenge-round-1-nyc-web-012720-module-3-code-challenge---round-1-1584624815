2f33ae3dace72e468f0c16728742f994c57f8b88
aprilfaithdays <aprilfaith.e@gmail.com>
2020-03-19 10:03:17 -0400

    second

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
+let imageId = 4880 //Enter the id from the fetched image here
+const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+const likeURL = `https://randopic.herokuapp.com/likes/`
+const commentsURL = `https://randopic.herokuapp.com/comments/`
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
-
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
-
-  const likeURL = `https://randopic.herokuapp.com/likes/`
-
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
 
   getImage()
 
 })
 
 
+
+
+
 const getImage = () => {
-  // use a GET fetch to 
+  // use a GET fetch to grab the image
+  // create a function that renders that image onto the page
+  fetch(imageURL)
+  .then(res => res.json())
+  .then(image => renderImage(image))
+}
 
 
+const imageCard = document.getElementById("image_card")
+
+const renderImage = image => {
+  const imageTag = imageCard.querySelector('img')
+  imageTag.src = image.url
+  imageTag.dataset.id = image.id
+
+  const nameTag = imageCard.querySelector('#name')
+  nameTag.innerText = image.name
+
+  console.log(image)
+  console.log(imageCard)
 }
