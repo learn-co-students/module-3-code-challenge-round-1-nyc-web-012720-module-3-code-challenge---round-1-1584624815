5005c88c51888624bd2e4595aff84707737dce6a
sekoudosso82 <sekou.dosso82@gmail.com>
2020-03-27 09:05:49 -0400

    pratice for retake

--- src/index.js
+++ src/index.js
@@ -8,99 +8,108 @@ console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
   const likeURL = `https://randopic.herokuapp.com/likes/`
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
-
-  // fetch all images
-  // document.getElementById('comment_input').value=null
-  let body = document.getElementsByTagName('body')
-  let name = document.getElementById("name")
+  let img = document.getElementById('image')
+  let imgName = document.getElementById('name')
+  let likes = document.getElementById('likes')
   let ul = document.getElementById('comments')
 
-   fetchImages()
+  fetchImage()
 
-    function fetchImages(){
-      fetch(imageURL)
-      .then(res => res.json())
-      .then(function(image){
-          addImage(image)
-      })
-    }
-
-    // add image to the browser
-    function addImage(image){
-      let img = document.getElementById("image")
-      img.dataset.beef = image.id
-      img.src = image.url 
-      name.innerText = image.name
-      likes.innerText = image.like_count
-      let comments = image.comments
-      comments.forEach(function(comment){
-        createComment(comment, ul)
-      })
-    }
-
-    function createComment(comment, ul){
-      let li = document.createElement('li')
-      li.innerHTML = `<h4>content: ${comment.content}</h4>`
-      ul.appendChild(li)
-    }
-
-    // ***************** increase like
-    document.addEventListener('click', function(event){
-      if (event.target.innerText === "Like"){
-        increaseLike()
-      }
+  function fetchImage(){
+    fetch(imageURL)
+    .then(res => res.json())
+    // .then(console.log)
+    .then(image => {
+      addImageToBrowser(image)
     })
-    
-    function increaseLike(){
-      let likesValue = parseInt(likes.innerText)
-      likesValue++
-      likes.innerText = likesValue
-      let id = document.getElementById('image').dataset.beef
-      updateLikeDb(likesValue, id)
-    }
-    
-    function updateLikeDb(likesValue, id) {
-      fetch(likeURL, {
-        method: "POST",
-        headers:{
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({image_id: id, like_count: likesValue})
-      })
-      .then(res => res.json())
-      .then(console.log)
-    }
+  }
+  function addImageToBrowser(image){
+    // let img = document.getElementById('image')
+    img.src = image.url 
+    img.dataset.id = image.id
+    // let imgName = document.getElementById('name')
+    imgName.innerHTML = image.name
+    // let likes = document.getElementById('likes')
+    likes.innerHTML = image.like_count
 
-    // ******************** add comments
-    document.addEventListener('submit', function(event){
-      event.preventDefault()
-      console.log('submit was clicked')
-      addCommentToBrowser()  
-    
-    
+    let comments = image.comments
+    comments.forEach(comment => {
+      addCommentTOBrowser(comment)
     })
+  }
+  
+  function addCommentTOBrowser(comment) {
+    // let ul = document.getElementById('comments')
+    let li = document.createElement('li')
+    li.innerHTML = `<h3> ${comment.content} </h3>`
+    ul.appendChild(li)
+  }
 
-    function addCommentToBrowser(){
-      let comment = document.getElementById('comment_input').value
-      let image_id = parseInt(document.getElementById("image").dataset.beef)
-      let newComment = { content: comment, image_id: image_id }
-      createComment(newComment, ul)
-      let id = document.getElementById('image').dataset.beef
-      
-      addCommentToDb(comment, id )
-      document.getElementById('comment_input').value=null
-    }
 
-    function addCommentToDb(comment, id ){
-      fetch(commentsURL, {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'},
-        body: JSON.stringify({image_id:id, content: comment})
-      })
-      .then(res => res.json())
-      .then(console.log)
-    }
+// ************** increase like in browser 
+document.addEventListener('click', function(event){
+  if (event.target.innerText === 'Like'){
+    increaseLike(event.target)
+  }
+})
+function increaseLike(button){
+  let newLikeScore = parseInt(likes.innerHTML) 
+  newLikeScore++
+  likes.innerHTML = newLikeScore
+  let id = button.parentNode.querySelector('img').dataset.id 
+  console.log(id)
+  updateLikeInDb(newLikeScore, id)
+}
+// ************** increase like in db 
+
+function updateLikeInDb(newLikeScore, id){
+  fetch(`https://randopic.herokuapp.com/likes/`,{
+    method: 'POST',
+    headers:{
+      'accept': 'application/json',
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({ 'image_id': id})
+
+  })
+  .then(res => res.json())
+  .then(console.log)
+}
+
+// ************** add comment in the browser
+
+document.addEventListener('submit', function(event) {
+    event.preventDefault()
+    console.log('submit was clicked')
+    createComment(event.target)
+})
+
+function createComment(button){
+  let comment = document.getElementById('comment_input').value
+  let image_id = parseInt(document.getElementById('image').dataset.id)
+  // let content = button.name.value 
+  console.log(image_id)
+  console.log(comment)
+  let newComment = {content: comment, image_id: image_id}
+
+  addCommentTOBrowser(newComment)
+  document.getElementById('comment_input').value = null
+  addCommentToDb(newComment)
+}
+
+// ************** add comment in the db
+
+function addCommentToDb(comment){
+  fetch(`https://randopic.herokuapp.com/comments/`,{
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    },
+    body: JSON.stringify({ image_id: comment.image_id, content: comment.content})
+  })
+  .then(res => res.json())
+  .then(console.log)
+}
+
 })
